a68d01d4ac97927ab3c306720690c2bf4e092d87
fd-Martin <fdmosharof@gmail.com>
2023-11-25 20:33:46 +0600

    error page has created

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+import errorImg from '../Error/pngwing.com.png';
+const Error = () => {
+    const error = useRouteError();
+    // console.log(error);
+    return (
+        <div>
+            <div className='max-w-4xl mx-auto px-5 py-10 text-center'>
+                <div className='mx-auto w-full p-5'>
+                    <img src={errorImg} alt="" className='mx-auto w-full' />
+
+                </div>
+                <div>
+
+                    <h1 className='text-6xl font-bold mb-5'>Oops!</h1>
+                    <p className='text-4xl font-semibold'>Sorry, an unexpected error has occurred.</p>
+                    <p className='text-2xl font-semibold'>
+                        {error.statusText || error.message}
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Error;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h1 className="text-7xl font-bold underline">
+                Hello world!
+            </h1>
+
+
+            <button class="btn btn-primary">One</button>
+            <button class="btn btn-secondary">Two</button>
+            <button class="btn btn-accent btn-outline">Three</button>
+        </div>
+    );
+};
+
+export default Main;
